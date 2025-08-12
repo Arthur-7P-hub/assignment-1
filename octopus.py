0f1b0250490f5167a913bd23b27a7d81a3451f35
Arthur-7p-hub <arthurkariuki1963@gmail.com>
2025-08-12 19:47:39 +0300

    added octopus.py

---
+++ octopus.py
@@ -0,0 +1,37 @@
+#PLP = "Academy"
+Student_name = "arthur pendragon"
+Student_ID = "1001"
+Student_Age = "30"
+Genius = True
+
+#Output
+print(Student_name, Student_ID, Student_Age, Genius)
+
+#lists
+Languages = ["python", "i995", "java", "c++"]
+print(Languages)
+
+shopping_list = ["Milk", "Unga"]
+print(shopping_list)
+
+#tuples
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+#sets
+Set_A = {1, 2, 3, 4}
+Set_B = {4, 5, 6, 7, 8}
+print(Set_A)
+print(Set_B   )
+
+#Dictionaries
+student_info = ("name", "arthur pendragon", "ID",)
+print(student_info)
+
+#logical Operators
+a = 10
+b = 20
+c = 30
+print(a < b and b > c) # True
+print(a > b and b < a) # False
+print(a < b or b < a) #true
